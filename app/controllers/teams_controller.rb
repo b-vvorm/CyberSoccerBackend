bfc1c2f3c866b9d8ed1d97db90b085d9fa278123
Vladimir <vl.shidlovsky@gmail.com>
2010-04-23 01:54:38 +0300

    implement team creation and editing

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -4,14 +4,27 @@ class TeamsController < ApplicationController
   layout 'game'
 
   def index
-
   end
 
-  def grid_data
-    teams = [Team.new(:name => "ManUtd"), Team.new(:name => "Liverpool"), Team.new(:name => "Arsenal"), Team.new(:name => "Chelsea")]
-    respond_to do |format|
-      format.html
-      format.json { render :json => teams.to_jqgrid_json([:name], params[:page], params[:rows], teams.length) }
+  def new
+    @team = Team.new
+  end
+
+  def create
+    @team = Team.new(params[:team])
+    if @team.save
+      redirect_to teams_url
+    else
+      render :action => :new
+    end
+  end
+
+  def update
+    @team = Team.find(params[:id])
+    if @team.update_attributes(params[:team])
+      redirect_to teams_url
+    else
+      render :action => :edit
     end
   end
 
@@ -19,6 +32,18 @@ class TeamsController < ApplicationController
   end
 
   def edit
+    @team = Team.find(params[:id])
+  end
+
+  def destroy
+  end
+
+  def grid_data
+    teams = Team.find_all_by_user_id @current_user.id
+    respond_to do |format|
+      format.html
+      format.json { render :json => teams.to_jqgrid_json([:name], params[:page], params[:rows], teams.length) }
+    end
   end
 
 end
--- app/helpers/jquery_grid_helper.rb
+++ app/helpers/jquery_grid_helper.rb
@@ -4,19 +4,14 @@ module JqueryGridHelper
   DEFAULT_GRID_WIDTH = 200
 
   # Includes jquery grid javascripts.
-  def javascript_include_jqgrid
+  def jqgrid_includes
     locale = I18n.locale rescue :en
-#    js = ''
-#    js << javascript_include_tag("jqgrid/i18n/grid.locale-#{locale}.js") + "\n"
-#    js << javascript_include_merged(:grid) + "\n"
-
-
     javascript_merged "jqgrid/i18n/grid.locale-#{locale}", "grid"
-
+    stylesheet_merged :jqgrid
   end
 
   # Renders helper javascript method for grid with new, edit, view and delete buttons.
-  def jquery_custom_grid(fields, options = {}, grid_name = controller_name)
+  def jquery_custom_grid(fields, options = {}, title = "", grid_name = controller_name)
 
     # Default options
     options = {
@@ -25,24 +20,27 @@ module JqueryGridHelper
       :error_handler => "gridErrorHandler",
       :rownumbers => true,
       :delete => false,
-      :edit => true,
-      :add => true,
-      :view => true,
+      :edit => false,
+      :add => false,
+      :view => false,
+      :search => false
     }.merge(options)
 
     show_add = options.delete(:add)
     show_view = options.delete(:view)
     show_edit = options.delete(:edit)
+    show_delete = options.delete(:delete)
     width = get_width(options, fields)
     row_buttons = options.delete(:row_buttons) || []
     grid_buttons = options.delete(:grid_buttons) || []
-    row_buttons += get_row_buttons(show_view, show_edit)
+    row_buttons += get_row_buttons(show_view, show_edit, show_delete)
     grid_buttons += get_grid_buttons(show_add)
 
-    #fields << { :field => "action_view", :label => "", :width => 20, :search => false } if show_view
-    fields << { :field => "action_edit", :label => "", :width => 20, :search => false } if show_edit
+    fields << { :field => "action_view", :label => "", :width => 14, :search => false } if show_view
+    fields << { :field => "action_edit", :label => "", :width => 14, :search => false } if show_edit
+    fields << { :field => "action_delete", :label => "", :width => 14, :search => false } if show_delete
 
-    output = jqgrid("", grid_name, url_for(:action => "grid_data"), fields, options)
+    output = jqgrid(title, grid_name, url_for(:action => "grid_data"), fields, options)
 
     output << %Q(
       <script type="text/javascript">
@@ -82,7 +80,7 @@ module JqueryGridHelper
     width = fields.inject(DEFAULT_GRID_WIDTH) {|sum, field| sum + field[:width]} unless width
   end
 
-  def get_row_buttons(show_view, show_edit)
+  def get_row_buttons(show_view, show_edit, show_delete)
     buttons = []
     if show_view
       buttons << {
@@ -99,6 +97,15 @@ module JqueryGridHelper
         :title => t("edit", :scope => @controller.controller_path.split("/")),
         :icon => "ui-icon-pencil",
         :column => "action_edit"
+      }
+    end
+
+    if show_delete
+      buttons << {
+        :path => url_for(:action => "destroy", :id => ":id", :method => "DELETE"),
+        :title => t("delete", :scope => @controller.controller_path.split("/")),
+        :icon => "ui-icon-trash",
+        :column => "action_delete"
       }
     end
     buttons
