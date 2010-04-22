0fc5452fda6e2d0915f4b85dc557fa34fc08003b
Vladimir <vl.shidlovsky@gmail.com>
2010-04-22 11:12:05 +0300

    add jquery grid

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -4,6 +4,11 @@ class TeamsController < ApplicationController
   layout 'game'
 
   def index
+    teams = [Team.new(:name => "ManUtd"), Team.new(:name => "Liverpool"), Team.new(:name => "Arsenal"), Team.new(:name => "Chelsea")]
+    respond_to do |format|
+      format.html
+      format.json { render :json => teams.to_jqgrid_json([:id,:name], params[:page], params[:rows], teams.length) }
+    end
   end
 
 end
