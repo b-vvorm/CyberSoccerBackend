f22aae4343223806c59b9681d4933d65ce3818c3
Vladimir <vl.shidlovsky@gmail.com>
2010-04-26 01:30:26 +0300

    add slider control

--- public/javascripts/base_packaged.js
+++ public/javascripts/base_packaged.js
@@ -5,4 +5,5 @@ function addGridButton(grid,placeHolder,buttons){if(!window.isAddGridButtonExecu
 button.onClickButton=eval("("+button.onClickButton+")")
 $(grid).navButtonAdd(placeHolder,buttons[b]);}}}
 function addGridRowButtons(grid,buttons){var ids=$(grid).jqGrid('getDataIDs');for(var i=0;i<ids.length;i++){var cl=ids[i];for(var b in buttons){button=buttons[b]
-link='<a title="'+button.title+'" href="'+button.path.replace(/:id/,cl)+'"><span class="ui-icon '+button.icon+'"></span></a>';options={};options[button.column]=link;$(grid).jqGrid('setRowData',ids[i],options);}}}
+if(button.path){link='<a title="'+button.title+'" href="'+button.path.replace(/:id/,cl)+'"><span class="ui-icon '+button.icon+'"></span></a>';}else if(button.func){link='<a title="'+button.title+'" href="javascript:'+button.func.replace(/:id/,cl)+'"><span class="ui-icon '+button.icon+'"></span></a>';}
+options={};options[button.column]=link;$(grid).jqGrid('setRowData',ids[i],options);}}}
